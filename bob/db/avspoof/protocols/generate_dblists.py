52bf8f65cf059e9e4134ea117c631c7b7defa559
Pavel Korshunov <pavel.korshunov@idiap.ch>
2015-12-11 10:46:21 +0100

    Fixing issue #3 if using not Py3 safe print

--- bob/db/avspoof/protocols/generate_dblists.py
+++ bob/db/avspoof/protocols/generate_dblists.py
@@ -17,8 +17,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+The script generates text file lists for grandtest protocol using the file names and
+the directory structure of the AVspoof database. These lists are later used to build an SQL-based
+bob.db interface for AVspoof database.
+"""
+from __future__ import print_function
+
 import os
-#import numpy
 import math
 import argparse
 
@@ -30,14 +36,14 @@ def listFiles(rootdir="."):
         path = path.split(os.path.sep)
         gender = "male"  # female also ends with "male", so we cover both
         if gender in root:
-            for file in files:
-                    file, ext = os.path.splitext(file)
-                    if ext == ".wav":
-                        subpath = path[1:]
-                        subpath.append(file)
-                        start_id_indx = root.index(gender)+len(gender)+1
-                        id = root[start_id_indx:start_id_indx+4]
-                        objs.append((os.path.join(*subpath), id))
+            for sample in files:
+                sample, ext = os.path.splitext(sample)
+                if ext == ".wav":
+                    subpath = path[1:]
+                    subpath.append(sample)
+                    start_id_indx = root.index(gender) + len(gender) + 1
+                    id = root[start_id_indx:start_id_indx + 4]
+                    objs.append((os.path.join(*subpath), id))
     return objs
 
 
@@ -57,18 +63,19 @@ def getSubIds(ids, set="train"):
     if not ids:
         return subids
 
-#  indices=numpy.arange(len(ids))
+    #  indices=numpy.arange(len(ids))
 
-    sublen = int(math.floor(len(ids)/3))
+    sublen = int(math.floor(len(ids) / 3))
 
     if set == "train":
         subids = ids[0:sublen]
     elif set == "devel":
-        subids = ids[sublen:2*sublen]
+        subids = ids[sublen:2 * sublen]
     elif set == "test":
-        subids = ids[2*sublen:]
+        subids = ids[2 * sublen:]
 
     return subids
+
 
 #  numpy.choice(ids)
 
@@ -78,7 +85,7 @@ def command_line_arguments(command_line_parameters):
 
     # set up command line parser
     parser = argparse.ArgumentParser(description=__doc__,
-                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('-d', '--database-directory', required=False,
                         help="The root directory of database data.")
@@ -90,7 +97,8 @@ def command_line_arguments(command_line_parameters):
 
 
 def main(command_line_parameters=None):
-    """Reads score files, computes error measures and plots curves."""
+    """Traverses the folder structure of AVspoof database and create file lists corresponding
+    to train, devel, and test sets indicating genuine or spoofed samples."""
 
     args = command_line_arguments(command_line_parameters)
 
@@ -98,66 +106,52 @@ def main(command_line_parameters=None):
     if args.database_directory:
         rootdir = args.database_directory
 
-    filters = ["genuine", "attack"]
+    data_types = ["genuine", "attack"]
     sets = ["train", "devel", "test"]
     prefix = "-grandtest-allsupports"
-#    prefix = "-smalltest-allsupports"
+    #    prefix = "-smalltest-allsupports"
 
-    # fix ids split for the whole thing, so we are consistent accross diff filters
-    male_ids = getIds(rootdir, "male", filters[0])
-    female_ids = getIds(rootdir, "female", filters[0])
+    # fix ids split for the whole thing, so we are consistent across diff filters
+    male_ids = getIds(rootdir, "male", data_types[0])
+    female_ids = getIds(rootdir, "female", data_types[0])
 
-    # traverse database and constract the whole list of files
-    list = listFiles(rootdir)
+    # traverse database and construct the whole list of files
+    file_list = listFiles(rootdir)
 
+    # write clients' Ids for train, devel, and test subsets into the file
     fid = open("clients%s.txt" % (prefix), "w")
     for set in sets:
         #   if not os.path.exists(set):
         #     os.makedirs(set)
-        set_ids = getSubIds(male_ids[0:3], set)
-        set_ids.extend(getSubIds(female_ids[0:3], set))
-        print "set %s, set_ids: %s " % (set, " ".join(id for id in set_ids))
-        fid.write('\n'.join("%s %s" % (id, set) for id in set_ids))
-        fid.write('\n')
-        for filter in filters:
+        set_ids = getSubIds(male_ids, set)
+        set_ids.extend(getSubIds(female_ids, set))
+        print("set %s, set_ids: %s " % (set, " ".join(client_id for client_id in set_ids)))
+        fid.write('\n'.join("%s %s" % (client_id, set) for client_id in set_ids))
+        fid.write('\n')  # wrote all client Ids
+        for data_type in data_types:
             # get ids for the specific set
             # set_ids = getSubIds(male_ids, set)
             # set_ids.extend(getSubIds(female_ids, set))
             # print "set %s, set_ids: %s " %(set, " ".join(id for id in set_ids))
-            # get only sublist for this set and this filter/type
+
+            # construct the sublist for the current set and the current data_type
             list4set = []
-#            counter=1
-            for item in list:
-                if item[1] in set_ids and filter in item[0]:
+            for item in file_list:
+                if item[1] in set_ids and data_type in item[0]:
                     list4set.append(item[0])
-#                    counter+=1
-#                    if counter == 5:
-#                        break
-#            print list4set
-            if filter == "genuine":
+
+            filename = ""
+            if data_type == "genuine":
                 filename = "real%s-%s.txt" % (prefix, set)
-            if filter == "attack":
+            if data_type == "attack":
                 filename = "attack%s-%s.txt" % (prefix, set)
-
+            # write file names either into real or attack file list
             with open(filename, "w") as f:
-                    f.write('\n'.join(stem for stem in list4set))
-                    f.close()
+                f.write('\n'.join(stem for stem in list4set))
+                f.close()
 
-
-#      else:
-#        if filter == "attack":
-#          filename=os.path.join(set, "for_scores.lst")
-#          with open(filename, "w") as f:
-#            for stem, id in list4set:
-#              stemsplit = stem.split(os.path.sep)
-#              f.write('%s %s %s %s\n' % (stem, id, id, stemsplit[1]) )
-#            f.close()
-#        if filter == "genuine":
-#          filename=os.path.join(set, "for_models.lst")
-#          with open(filename, "w") as f:
-#            f.write('\n'.join("%s %s %s" %(stem, id, id) for stem,id in list4set))
-#            f.close()
-#
     fid.close()
+
+
 if __name__ == '__main__':
     main()
